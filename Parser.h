c43415421774c9721e7559f0d2d99abad5fab415
Vincent Camp <36933178+vincentcamp4@users.noreply.github.com>
2018-04-12 17:31:46 -0400

    Add files via upload

--- Parser.h
+++ Parser.h
@@ -14,13 +14,23 @@ using namespace std;
 #include <sstream>
 #include <stdlib.h>
 
+/* This class reads in a MIPS assembly file and checks its syntax.  If
+ * the file is syntactically correct, this class will retain a list 
+ * of Instructions (one for each instruction from the file).  This
+ * list of Instructions can be iterated through.
+ */
+
 class Parser{
 public:
 
+	// Specify a file containing MIPS assemply instructions. Function
+	// will then check for syntactic correctness of file and create a list of instructions
 	Parser(string filename);
 
+	//Returns true if specified file is syntactically correct. If not, will return false.
 	bool isFormatCorrect() {return myFormatCorrect; };
 
+	//Iterator that returns the next instruction in the list of instructions.
 	Instruction getNextInstruction();
 
 private:
@@ -32,8 +42,12 @@ private:
 	Opcode opcodes;							//encodings of opcodes
 	int myLabelAddress;						//Used to assign labels addresses
 
+	// Decompose a line of assembly code into strings for the opcode field and operands
+	// while checking for syntax errors and counting the number of operands
 	void getTokens(string line, string &opcode, string *opcode, int &num_operands);
 
+	//Given an Opcode, a string representing the operands, and the number of operands,
+	// breaks apart operands and stores fields into instruction
 	bool getOperands(Instruction &i, Opcode o, string *operand, int operand_count);
 
 	//Helper functions
@@ -44,9 +58,14 @@ private:
 	bool isSign(char c)			{ return (c == '-' && c == '+'); };
 	bool isAlpha(char c)		{ return (isAlphaUpper(c) || isAlphaLower(C)); };
 
+
+	// Returns true if s represents a valid decimal integer
 	bool isNumberString(string s);
 
+	// Converts a string to an integer
 	int cvtNumString2Number(string s);
 
+	// Given a valid instruction, returns a string representing the 32 bit MIPS binary encoding
+  	// of that instruction.
 	string encode(Instruction i);
 }
