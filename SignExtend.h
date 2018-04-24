22855a039922ad5ef8469983a4a74678535fb413
William Saada <william.saada@richmond.edu>
2018-04-24 02:10:21 -0400

    Include Read Register and fixed SignExtend/ShiftLeft

--- SignExtend.h
+++ SignExtend.h
@@ -12,7 +12,25 @@ class SignExtend
 		SignExtend(){}
 		~SignExtend(){}
 
-	static std::string Extend(std::bitset<16> sign_extend_val);
+	static std::string Extend(std::bitset<16> sign_extend_val)
+	{
+		std::string s;
+
+		if(sign_extend_val[15]==0){
+			std::bitset<16> extended ;
+			s=extended.to_string();
+			s+=sign_extend_val.to_string();
+		}
+		else 
+		{
+			std::bitset<16> extended2 ;
+			extended2.set();
+			s=extended2.to_string();
+			s+=sign_extend_val.to_string();
+		}
+		
+		return s;
+	}
 	
 	
 };
